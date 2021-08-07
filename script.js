1c6351e37bead5b524a0afc9967aea3fc20b0dd4
Deepesh Dhakal <deepesh.dhakal@thamserku.com>
2021-08-07 12:39:36 +0545

    calendar ready

--- script.js
+++ script.js
@@ -8,39 +8,82 @@ const dt = new Date();
  * Render Calendar main function
  */
 function render_calendar(){
+	const year = dt.getFullYear();
+	const month = dt.getMonth();
+	const date = dt.getDate();
+	const day = dt.getDay();
+	// console.log(year,month,date,day);
+	//grab the calendar grid
+	const calendar_grid = document.querySelector('#calendar_grid');
+	//clear the calendar grid before rendering
+	calendar_grid.innerHTML = "";
+	
+	//previous month last day and no of days(last_date) grabber
+	const previous_month_object = new Date(year, month, 0);
+	const previous_month_last_date = previous_month_object.getDay();
+	const previous_month_last_day = previous_month_object.getDate();
+	// console.log(previous_month_last_date, previous_month_last_day);
 
-	console.log("calendar");
+
+	//loop over last months padding and add empty elements
+	const padding_day_start_index = previous_month_last_day - previous_month_last_date;
+	// console.log(padding_day_start_index);
+	for(let y=padding_day_start_index; y<= previous_month_last_day; y++){
+		calendar_grid.innerHTML += `<div class="padding_days">${y}</div>`;
+	}
+
+	//current month last day and no of days grabber
+	const currentMonthObject = new Date(year, month+1, 0);
+	let no_of_days_current_month = currentMonthObject.getDate();
+	let last_day_of_current_month = currentMonthObject.getDay();
+	
+	//loop over days to render all days in the calendar
+	for(let x = 1; x<=no_of_days_current_month; x++){
+		calendar_grid.innerHTML += `<div class="days">${x}</div>`;
+	}
+	
+	// loop over and pad next month days
+	console.log(no_of_days_current_month, last_day_of_current_month);
+	const remaining_padding_space = 6 - last_day_of_current_month;
+	for(let z = 1; z<=remaining_padding_space; z++){
+		calendar_grid.innerHTML += `<div class="padding_days">${z}</div>`;
+	}
+
+
+	highlight_today();
+	display_date_information();
+	document.querySelector('#calendar_wrapper #header_wrapper .next_month').addEventListener('click', get_next_month);
+	document.querySelector('#calendar_wrapper #header_wrapper .prev_month').addEventListener('click', get_prev_month);
 }
 render_calendar();
 
-display_date_information();
 function display_date_information(){
 	const date_display = document.querySelector('#date_display');
 	date_display.innerHTML = dt.toDateString();
 }
 
-highlight_today();
+
 function highlight_today(){
 	const calendar_grid_days = document.querySelectorAll('#calendar_grid .days');
+	//grab current month index to check equality with month in the calendar
+	let current_month = new Date().getMonth();
 	calendar_grid_days.forEach((day)=>{
-		if(day.innerText == dt.getDate()){
+		if(day.innerText == dt.getDate() && current_month == dt.getMonth()){
 			day.classList.add('today');
 		}
 	})
 }
 
-document.querySelector('#calendar_wrapper #header_wrapper .next_month').addEventListener('click', get_next_month);
 
 // get Next Month
 function get_next_month(){
 	dt.setMonth(dt.getMonth()+1);
-	display_date_information();
+	render_calendar();
 }
 
 // get previous month
 
-document.querySelector('#calendar_wrapper #header_wrapper .prev_month').addEventListener('click', get_prev_month);
 function get_prev_month(){
 	dt.setMonth(dt.getMonth()-1);
-	display_date_information();
+	render_calendar();
 }
